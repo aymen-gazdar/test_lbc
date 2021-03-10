f7c8921026393dff5f4f35690a205c0edc741300
Aymen <aymen.mg3@gmail.com>
2021-03-10 21:51:12 +0100

    PrepareCategoryScreen: adding category screen with a tableview + adding barButton. [DONE]

--- TestLBC/Extensions.swift
+++ TestLBC/Extensions.swift
@@ -7,18 +7,4 @@
 
 import UIKit
 
-/**
-    AutoLayout Property Wrapper :
-        allowing to do not constantly writing
-        view.translatesAutoresizingMaskIntoConstraints = false
- */
 
-@propertyWrapper
-public struct UseAutoLayout<T: UIView> {
-    public var wrappedValue: T
-
-    public init(wrappedValue: T) {
-        self.wrappedValue = wrappedValue
-        wrappedValue.translatesAutoresizingMaskIntoConstraints = false
-    }
-}
